857c95e7a152e8e0c99789f3bb87a2e6ef7571c1
John Hoerr <jhoerr@gmail.com>
2021-03-01 21:02:31 -0500

    Final version of readonly converter

--- console-apps/boxaccount-read-only/Program.cs
+++ console-apps/boxaccount-read-only/Program.cs
@@ -15,36 +15,95 @@ namespace boxaccount_read_only
 {
     class Program
     {
-        
-        static async Task  Main(string[] args)
+        const string LogFilePath = "/path/to/logfile.csv";
+        static string[] ExclusionList = new string[]{
+            // put in here the usernames of any accounts you wish to 
+            // exclude from the readonly conversion. 
+        };
+        const int MaxConcurrentUpdates = 8;
+        static string[] UserFields = new[]{"id", "name", "login", "status"};
+        private const string BoxReadOnlyStatus = "cannot_delete_edit_upload";
+
+        static async Task Main(string[] args)
         {
-                      
+            ConsoleLog("Creating Box admin client...");
+            BoxClient boxAdminClient = CreateBoxAdminClient();
+
+            ConsoleLog("Fetching enterprise users...");
+            var enterpriseUsers = await boxAdminClient.UsersManager.GetEnterpriseUsersAsync(limit: 1000, fields: UserFields, autoPaginate: true);
+
+            ConsoleLog($"Filtering {enterpriseUsers.Entries.Count()} users...");
+            var filteredUsers = enterpriseUsers.Entries
+                .OrderBy(u => u.Login)
+                .Where(u => false == AccountIsReadOnly(u))
+                .Where(u => false == AccountIsInactive(u))
+                .Where(u => false == AccountInExclusionList(u))
+                .ToList();
+
+            ConsoleLog($"Updating {filteredUsers.Count()} users...");
+            var startTime = DateTime.Now;
+            var processedCount = 0;
+            foreach (var users in UserSubSets(filteredUsers, MaxConcurrentUpdates))
+            {
+                var tasks = users.Select(u => UpdateUserStatus(u, boxAdminClient)).ToList();
+                await Task.WhenAll(tasks);
+                processedCount += tasks.Count();
+                if (processedCount % (MaxConcurrentUpdates * 10) == 0)
+                {
+                    // log stats every 10 rounds
+                    PrintTimeStats(startTime, filteredUsers.Count(), processedCount);
+                }
+                await LogResults(tasks);
+                await Task.Delay(100);
+            }
+
+            PrintTimeStats(startTime, filteredUsers.Count(), processedCount);
+            ConsoleLog("Done!");
+        }
+
+        private static bool AccountIsInactive(BoxUser u) 
+            => u.Status == "inactive";
+        private static bool AccountIsReadOnly(BoxUser u) 
+            => u.Status == BoxReadOnlyStatus;
+
+        private static bool AccountInExclusionList(BoxUser u)
+        {
+            var username = u.Login.Split("@", StringSplitOptions.RemoveEmptyEntries).First();
+            return ExclusionList.Any(dnt => string.Equals(dnt, username, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private static async Task LogResults(List<Task<UpdateResult>> tasks)
+        {
+            var lines = tasks.Select(t => t.Result.ToString());
+            await File.AppendAllLinesAsync(LogFilePath, lines);
+        }
+
+        private static void PrintTimeStats(DateTime startTime, int totalCount, int processedCount)
+        {
+            var timePerUserInMs = Math.Ceiling((float)(DateTime.Now - startTime).TotalMilliseconds / processedCount);
+            var usersRemaining = totalCount - processedCount;
+            var timeRemainingInMin = Math.Ceiling((timePerUserInMs * usersRemaining) / (1000 * 60));
+            ConsoleLog($"Processed {processedCount} of {totalCount}. {timePerUserInMs} ms/user. Est {timeRemainingInMin} mins remaining.");
+        }
+
+        private static void ConsoleLog(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString("s")}] {msg}");
+        }
+
+        private static BoxClient CreateBoxAdminClient()
+        {
             var appConfig = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
             var configJson = appConfig["BoxConfigJson"];
-            
-            Console.WriteLine("Creating Box admin client...");
+            if (string.IsNullOrWhiteSpace(configJson))
+                throw new Exception("Missing user secret: BoxConfigJson");
             var config = BoxConfig.CreateFromJsonString(configJson);
             var auth = new BoxJWTAuth(config);
             var adminToken = auth.AdminToken();
-            var boxAdminClient = auth.AdminClient(adminToken);  
+            var boxAdminClient = auth.AdminClient(adminToken);
+            return boxAdminClient;
+        }
 
-            BoxCollection<BoxUser> enterpriseUsers ;     
-            uint offset = 0;      
-            do
-            {
-                enterpriseUsers = await boxAdminClient.UsersManager.GetEnterpriseUsersAsync(limit: 1000, offset: offset);
-                offset += Convert.ToUInt32(enterpriseUsers.Entries.Count());
-                var filteredUsers = enterpriseUsers.Entries.Where(u=>u.Status != "cannot_delete_edit_upload");
-                var userGroups = UserSubSets(filteredUsers, 10);
-                foreach(var users in userGroups) {
-                    var tasks = users.Select(u=> System.Console.Out.WriteLineAsync($"{DateTime.Now.ToString("ss.fff")} UserId:{u.Id}"));
-                    await Task.WhenAll(tasks);
-                    await Task.Delay(100);
-                    
-                    //await UpdateUserStatus(user.Id, boxAdminClient); 
-                }
-            } while (offset < enterpriseUsers.TotalCount);  
-        }  
         public static IEnumerable<List<T>> UserSubSets<T>(IEnumerable<T> enterpriseUsers, int max)
         {
             List<T> toReturn = new List<T>(max);
@@ -63,13 +122,42 @@ namespace boxaccount_read_only
             }
         }    
 
-        private static async Task UpdateUserStatus(string boxUserId, BoxClient boxAdminClient)
+        private class UpdateResult
         {
-            await boxAdminClient.UsersManager.UpdateUserInformationAsync(new BoxUserRequest()
+            public UpdateResult(BoxUser user, Exception ex = null)
             {
-                Id = boxUserId,
-                Status = "cannot_delete_edit_upload"
-            });
+                User = user;
+                Timestamp = DateTime.Now;
+                Exception = ex;
+                Success = ex == null;
+            }
+
+            public BoxUser User { get; }
+            public DateTime Timestamp { get; }
+            public bool Success { get; }
+            public Exception Exception { get; }
+            public string Status { get; }
+
+            public override string ToString() 
+                => $"{Timestamp.ToString("s")},{User.Login},{User.Name},{User.Status},{(Exception == null ? string.Empty : Exception.ToString())}";
+        }
+        private static Random Rnd = new Random();
+        private static async Task<UpdateResult> UpdateUserStatus(BoxUser user, BoxClient boxAdminClient)
+        {
+            try
+            {
+                await Task.Delay(Rnd.Next(0,25));
+                await boxAdminClient.UsersManager.UpdateUserInformationAsync(new BoxUserRequest()
+                {
+                    Id = user.Id,
+                    Status = BoxReadOnlyStatus
+                });
+                return new UpdateResult(user);                
+            }
+            catch (System.Exception ex)
+            {
+                return new UpdateResult(user, ex);
+            }
         }
     }
 }
